8ffc54d4a103ea7649a5b30a5d09262f9048c2f8
Brian-with <chiyoungv@gmail.com>
2022-05-09 21:15:45 +0900

    ADD : Models.py

--- my_settings.py
+++ my_settings.py
@@ -3,7 +3,7 @@ SECRET_KEY = 'django-insecure-9%a31$!&7_aqys0p21bz!bprju$3dkexo#u52$_b&#j$a_%n6g
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB_MasterAndPuppy',
+        'NAME': 'test_starbucks',
         'USER': 'root',
         'PASSWORD': '07280728',
         'HOST': '127.0.0.1',
--- products/views.py
+++ products/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+import json
+from django.views import View
 
-# Create your views here.
+from .models import Category, Product, Nutrition, Menu
+class ProductsView(View):
+    pass
+    # def post(self, request):
+        
+    #     목적    : Client에서 보내는 상품정보(category, korean_name, english_name, description, protein, size, sodium)을
+    #             database의 products 테이블에 저장한다.
+        
+        
+    #     input   :
+    #         {
+    #             "menu": "drinks"
+    #             "catagory" : "cold brew",
+    #             "description" : "test",
+    #             "korean_name" : "나이트로 바닐라 크림",
+    #             "english_name" : "Nitro Vanila Cream",
+    #             "protein": 10.3,
+    #             "size" : 60.5,
+    #             "sodium": 10.5,
+    #         }
+        
+    #     input_data = json.loads(request.body) # use json, convert to dictionary
+        
+    #     menu= Menu.objects.create(name=input_data["menu"])
+    #     category = Category.objects.create(name=input_data["category"], menu=menu)
+    #     nutrition = Nutrition.objects.create(
+    #         protein_g = input_data["protein"],
+    #         sodium_mg = input_data["sodium"],
+    #         size_ml = input_data["size"]
+    #     )
+    #     product = Product(
+    #         korean_name = ,
+    #         english_name = ,
+    #         description = ,
+
+
+    #     )
+
+    #         Category 모델 사용, category 테이블에 row를 추가
+    #         Nutrition 모델 사용, nutrition table (protein, size, sodium)
+    #         Product 모델 사용
+            
+    #     output  :
+    #         {
+    #             "message" : "SUCCESS"
+    #         }, status_code = 201
+
+        
+        
+        
+
+    # def get(self, request):
+    #     pass
