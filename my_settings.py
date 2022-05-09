150df122e1f33107312c65f0ed077fdb38f953cf
Brian-with <chiyoungv@gmail.com>
2022-05-09 20:45:56 +0900

    CREATE : 프로젝트 초기 셋팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-9%a31$!&7_aqys0p21bz!bprju$3dkexo#u52$_b&#j$a_%n6g'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DB_MasterAndPuppy',
+        'USER': 'root',
+        'PASSWORD': '07280728',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
